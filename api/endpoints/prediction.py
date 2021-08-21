5a04f8d4bcf8150849959bf9688663cf8ac18d31
ropamon1202 <ropamon1202@gmail.com>
2021-08-21 14:33:58 -0400

    Updating README

--- api/endpoints/prediction.py
+++ api/endpoints/prediction.py
@@ -2,9 +2,9 @@ import pandas as pd
 from flask import request, jsonify, Blueprint
 import pickle
 from data_prep import prep_data
+
 # Create Flask Blueprint
 prediction_api = Blueprint('prediction_api', __name__)
-
 
 # Load model
 model = pickle.load(open('model/model.pkl', 'rb'))
@@ -16,15 +16,12 @@ def predict():
     # Request json data
     data = pd.DataFrame(request.json)
 
+    # Prepare the data for model
     data = prep_data(data)
 
-    # Check that number of variables is 25
-    if data.shape[1] == 25:
-        # Make prediction
-        prediction = model.predict(data)
-        # Create labels based on predictions (assuming threshold of 0.5)
-        label = [1 if i >= 0.5 else 0 for i in prediction]
-        # Return the required result
-        return jsonify({'Probability': list(prediction), 'Label': list(label), 'Variables': list(data.columns)})
-    else:
-        return "Number of variables != 25"
+    # Make prediction
+    prediction = model.predict(data)
+    # Create labels based on predictions (assuming threshold of 0.5)
+    label = [1 if i >= 0.5 else 0 for i in prediction]
+    # Return the required result
+    return jsonify({'Probability': list(prediction), 'Label': list(label), 'Variables': list(data.columns)})
